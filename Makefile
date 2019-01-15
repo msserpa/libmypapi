f2d72521d3bd329df15437bf5dbe31f5cf9f8e59
msserpa <msserpa@inf.ufrgs.br>
2019-01-15 15:36:52 -0200

    overflow handling

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC=gcc
-CFLAGS=-O2 -fPIC -ggdb -Wall -I/usr/local/include
-LDFLAGS=-lpthread -shared -Wl,--no-as-needed -ldl -L/usr/local/lib/ -lpapi
+CFLAGS=-O2 -fPIC -Wall -I/usr/local/include
+LDFLAGS=-shared -Wl,--no-as-needed -ldl -L/usr/local/lib/ -lpapi
 
 libnamedyn=libmypapi.so
 
--- mypapi.c
+++ mypapi.c
@@ -1,37 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 #include <unistd.h>
 #include <papi.h>
 
 char *event, exe[2048];
 int retval, length, EventSet1 = PAPI_NULL;
 long long value[2];
+long long overflows = 0;
 
 /* Use a positive value of retval to simply print an error message */
-void test_fail(const char *file, int line, const char *call, int retval){
-	char buf[128];
-	(void)file;
-
-	memset(buf, '\0', sizeof(buf));
-
-	fprintf(stderr, "FAILED!!!");
-	fprintf(stderr, "\nLine # %d ", line);
-
-	if(retval == PAPI_ESYS){
-		sprintf(buf, "System error in %s", call);
-		perror(buf);
-	}else if(retval > 0)
-		fprintf(stdout, "Error: %s\n", call);
-	else if(retval == 0)
-		fprintf(stderr, "Error: %s\n", call);
+void error_handler(int line, const char *call, int retval){
+	if(retval >= 0 || retval == PAPI_ESYS)
+		fprintf(stderr, "Error: %s on line %d\n", call, line);
 	else
-		fprintf(stderr, "Error in %s: %s\n", call, PAPI_strerror(retval));
+		fprintf(stderr, "Error: %s : %s on line %d\n\n", call, PAPI_strerror(retval), line);
 
-	if(PAPI_is_initialized())
-		PAPI_shutdown();
+	_exit(1);
+}
 
-	exit(1);
+void overflow_handler(int EventSet, void *address, long_long overflow_vector, void *context){
+	overflows++;
+	if(overflows >= LLONG_MAX / (long long) INT_MAX)
+		error_handler(__LINE__, "overflow", 1);	
 }
 
 static void __attribute__ ((constructor)) constructor();
@@ -39,7 +31,7 @@ static void __attribute__ ((constructor)) constructor();
 static void constructor(){
 	length = readlink("/proc/self/exe", exe, sizeof(exe) - 1);
 	if(length == -1)
-        test_fail(__FILE__, __LINE__, "readlink", 1);
+        error_handler(__LINE__, "readlink", 1);
 
     /* Only programs whose name ends with ".x" are accepted */
 	if(exe[length - 2] == '.' && exe[length - 1] == 'x'){
@@ -47,29 +39,35 @@ static void constructor(){
 		/* Init the PAPI library */
 		retval = PAPI_library_init(PAPI_VER_CURRENT);
 		if(retval != PAPI_VER_CURRENT){
-			test_fail(__FILE__, __LINE__, "PAPI_library_init", retval);
+			error_handler(__LINE__, "PAPI_library_init", retval);
 		}
 
 		/* Initialize the EventSet */
 		retval = PAPI_create_eventset(&EventSet1);
 		if(retval != PAPI_OK)
-			test_fail(__FILE__, __LINE__, "PAPI_create_eventset", retval);
+			error_handler(__LINE__, "PAPI_create_eventset", retval);
 
 		/* Get environment variable */
 		event = getenv("PAPI_EVENT");
 		if(event == NULL)
-			test_fail(__FILE__, __LINE__, "PAPI_EVENT is not defined", 1);
+			error_handler(__LINE__, "PAPI_EVENT is not defined", 1);
 
 		/* Add event from ENV */
 		retval = PAPI_add_named_event(EventSet1, event);
 		if(retval != PAPI_OK){
-			test_fail(__FILE__, __LINE__, "Trouble adding event", retval);
+			error_handler(__LINE__, "Trouble adding event", retval);
 		}
+
+		int eventCode;
+		PAPI_event_name_to_code(event, &eventCode);
+  		retval = PAPI_overflow(EventSet1, eventCode, INT_MAX, 0, overflow_handler);
+  		if(retval != PAPI_OK)
+  			error_handler(__LINE__, "PAPI_EVENT is not defined", retval);
 
 		/* Start PAPI */
 		retval = PAPI_start(EventSet1);
 		if(retval != PAPI_OK)
-			test_fail(__FILE__, __LINE__, "PAPI_start", retval);
+			error_handler(__LINE__, "PAPI_start", retval);
 
 	}
 }
@@ -83,19 +81,19 @@ static void destructor(){
 		/* Stop PAPI */
 		retval = PAPI_stop(EventSet1, value);
 		if(retval != PAPI_OK)
-			test_fail(__FILE__, __LINE__, "PAPI_stop", retval);
+			error_handler(__LINE__, "PAPI_stop", retval);
 
 		/* Shutdown the EventSet */
 		retval = PAPI_remove_named_event(EventSet1, event);
 		if(retval != PAPI_OK)
-			test_fail(__FILE__, __LINE__, "PAPI_remove_named_event", retval);
+			error_handler(__LINE__, "PAPI_remove_named_event", retval);
 
 		retval = PAPI_destroy_eventset(&EventSet1);
 		if(retval != PAPI_OK)
-			test_fail(__FILE__, __LINE__, "PAPI_destroy_eventset", retval);
+			error_handler(__LINE__, "PAPI_destroy_eventset", retval);
 	
 		/* It checks if event name is cuda-based and split it */
-		int i, j;
+		unsigned int i, j;
 
 		for(i = 0; i < strlen(event); i++)
 			if(strncmp(&event[i], "event:", 6) == 0){
