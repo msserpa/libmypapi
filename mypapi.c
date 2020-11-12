459cd9951515d9e0446424a21de304c67bb2ec0d
Matheus Serpa <msserpa@inf.ufrgs.br>
2020-11-12 14:24:28 -0300

    fixing some issues

--- mypapi.c
+++ mypapi.c
@@ -3,9 +3,11 @@
 #include <string.h>
 #include <limits.h>
 #include <unistd.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <papi.h>
+#include <time.h>
 
 #define PAPI_MAX_EVENTS 10
 #define BUFFER_SIZE 1024
@@ -23,23 +25,28 @@ void error_handler(int line, const char *call, int retval){
 static void __attribute__ ((constructor)) constructor();
 
 static void constructor(){
-	char exe[BUFFER_SIZE], **list_events = NULL;
+	char short_exec[BUFFER_SIZE], exec[BUFFER_SIZE], file[BUFFER_SIZE], log_dir[BUFFER_SIZE];
+	char **list_events = NULL;
 	int i, retval, length, sample_delay_ms, EventSet1 = PAPI_NULL, num_events = 0;
 	long long int *values;
 	pid_t pid = 0;
+	FILE *f;
 
-	length = readlink("/proc/self/exe", exe, sizeof(exe) - 1);
+	/* Get full path */
+	length = readlink("/proc/self/exe", exec, sizeof(exec) - 1);
 	if(length == -1)
         error_handler(__LINE__, "readlink", 1);
 
+    /* Get only application name */
+    strcpy(short_exec,  strrchr(exec, '/') + 1);
+
     /* Only programs whose name ends with ".x" are accepted */
-	if(exe[length - 2] == '.' && exe[length - 1] == 'x'){
+	if(exec[length - 2] == '.' && exec[length - 1] == 'x'){
 		/* Get sample delay from environment variable */
 		if(getenv("PAPI_DELAY") == NULL)
 			sample_delay_ms = 250;
 		else
 			sample_delay_ms = atoi(getenv("PAPI_DELAY"));
-
 
 		/* Init the PAPI library */
 		retval = PAPI_library_init(PAPI_VER_CURRENT);
@@ -109,6 +116,34 @@ static void constructor(){
 			}
 		}
 
+		/* Output directories */
+		time_t t = time(NULL);
+		struct tm tm = *localtime(&t);
+
+		struct stat st = {0};
+
+		sprintf(log_dir, "./log");
+		if(stat(log_dir, &st) == -1)
+	    	mkdir(log_dir, 0700);
+
+		sprintf(log_dir, "./log/%04d-%02d-%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday);
+		if(stat(log_dir, &st) == -1)
+	    	mkdir(log_dir, 0700);
+
+		sprintf(log_dir, "%s/%02d-%02d-%02d/",log_dir, tm.tm_hour, tm.tm_min, tm.tm_sec);
+		if(stat(log_dir, &st) == -1)
+	    	mkdir(log_dir, 0700);
+
+	    /* Output file */
+	    sprintf(file, "%s/%s.freq", log_dir, short_exec);
+		f = fopen(file, "w");
+		if(f == NULL)
+			error_handler(__LINE__, "Trouble creating the output file", retval);
+
+		/* Register the timestamp and the file header */
+		fprintf(f, "%lu\n", (unsigned long) time(NULL));
+		fprintf(f, "event,value\n");
+
 		/* Start PAPI */
 		retval = PAPI_start(EventSet1);
 		if(retval != PAPI_OK)
@@ -130,9 +165,9 @@ static void constructor(){
 				if(retval != PAPI_OK)
 					error_handler(__LINE__, "PAPI_read", retval);
 
-				/* Printing the hardware counters */
+				/* Writting the hardware counters */
 				for(i = 0; i < num_events; i++)
-					printf("%s,%lld\n", list_events[i], values[i]);
+					fprintf(f, "%s,%lld\n", list_events[i], values[i]);
 
 				/* Reducing overhead with a delay between samples */
 				usleep((useconds_t) sample_delay_ms * 1000);
@@ -142,6 +177,9 @@ static void constructor(){
 				if(child == pid)
 					break;
 			}
+
+			/* Write the output file */
+			fclose(f);
 
 			/* Check child status */
 			if(WEXITSTATUS(status) != 0)
